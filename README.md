60a4cd3e1b22d763b46db431bdb1860d1fadb7d9
Soumi Sarkar <90122541+SoumiOnGit@users.noreply.github.com>
2023-12-27 09:54:13 +0000

    signup login

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # music-player
+
+**app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://users.db'**
+when the Flask application initializes SQLAlchemy or performs any database-related operations, it will use the specified SQLite database file (users.db) for storage. The SQLAlchemy ORM will then handle interactions with the database, allowing you to use Python classes and objects to represent database tables and records.
+
+
--- app.py
+++ app.py
@@ -1,3 +1,54 @@
 from flask import Flask , render_template , request , redirect , url_for 
-from flask_sqlalchemy import SQLALchemy
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite://users.db' # flask innitialises sqlalchemy for db operations , it will use the specified db sqlite files for storage (users.db)
+db = SQLAlchemy(app) #binding sql to flask app
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key= True)
+    username = db.Column(db.string(80), unique= True, nullable= False)
+    password = db.Column(db.string(20), nullable= False)
+    email = db.Column(db.string(50), unique= True, nullable= False)
+
+db.create_all()
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/signup' , methods = ['GET', 'POST']) #when you define a route or view function, you can use the methods attribute to specify which HTTP methods the route should handle. GET :Used to retrieve information from the server. POST : Used to submit data to be processed to a specified resource. Often used for creating or updating a resource on the server.
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+
+        if User.query.filter_by(username=username).first():  #query means to query the database for a user with a specific username . .first() means to retrieve the first result of query 
+            return 'username already exists , please choose another username '
+        new_user = User(username=username , password= password , email = email)
+        db.session.add(new_user)
+        db.session.commit()   
+
+        return redirect(url_for('login'))
+    return render_template('signup.html')
+
+
+@app.route('/login' , methods = ['GET', 'POST'])
+def login():
+    if request.method =='POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user = User.query.filter_by(username=username, password=password).first()
+
+        if user:
+            return f'Welcome , {user.username} !!'
+        else:
+            return 'Invalid login Credentials :( '
+        
+    return render_template('login.html')
+    
+if __name__=='main':
+    app.run(debug=True)
